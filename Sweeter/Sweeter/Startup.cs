82941e94fb82c0af93e8bb37be992853e97a9ef3
Rufield <biglazycatukr@gmail.com>
2018-01-15 12:51:39 +0200

    Closed #4

--- Sweeter/Sweeter/Startup.cs
+++ Sweeter/Sweeter/Startup.cs
@@ -27,8 +27,19 @@ namespace Sweeter
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            services.AddMvc();
+            services.AddMvc(o => o.Conventions.Add(new FeatureConvention()))
+                  .AddRazorOptions(options =>
+                  {
+                      // {0} - Action Name
+                      // {1} - Controller Name
+                      // {2} - Feature Name
+                      // Replace normal view location entirely
+                      options.ViewLocationFormats.Clear();
+                      options.ViewLocationFormats.Add("/Features/{2}/{1}/{0}.cshtml");
+                      options.ViewLocationFormats.Add("/Features/{2}/{0}.cshtml");
+                      options.ViewLocationFormats.Add("/Features/Shared/{0}.cshtml");
+                      options.ViewLocationExpanders.Add(new FeatureFoldersRazorViewEngine());
+                  });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
