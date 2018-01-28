c47e587713b7f609021cb1aa0c3793e12136f081
schect404 <tomilovych@dlit.dp.ua>
2018-01-28 18:27:18 +0200

    Added Account provider and controller

---
+++ Sweeter/Sweeter/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sweeter.Models;
+using Dapper;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using System.Data.SqlClient;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Sweeter.Controllers
+{
+    using Models;
+    using DataProviders;
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private IAccountDataProvider accountDataProvider;
+        public AccountController(IAccountDataProvider accountData)
+        {
+            this.accountDataProvider = accountData;
+        }
+
+        [HttpGet("{id}")]
+        public async Task Get(int id)
+        
+           => await this.accountDataProvider.GetAccount(id);
+
+        [HttpPost]
+        public async Task Post([FromBody]AccountModel product)
+        {
+            await this.accountDataProvider.AddAccount(product);
+        }
+
+
+    }
+}
