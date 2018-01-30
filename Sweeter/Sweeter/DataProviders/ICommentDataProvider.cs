520c8aeff60d891a0090d43dde73f26b34e84a39
schect404 <tomilovych@dlit.dp.ua>
2018-01-30 21:25:51 +0200

    Changed and added providers

---
+++ Sweeter/Sweeter/DataProviders/ICommentDataProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sweeter.DataProviders
+{
+    using Models;
+    using Dapper;
+
+  public  interface ICommentDataProvider
+    {
+        Task<IEnumerable<CommentModel>> GetComments();
+
+        Task<CommentModel> GetComment(int id);
+
+        Task AddComment(CommentModel comment);
+        Task UpdateComment(CommentModel comment);
+    }
+}
---
+++ Sweeter/Sweeter/DataProviders/PostDataProvider.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace Sweeter.DataProviders
+{
+    using Models;
+    public class PostDataProvider:IPostDataProvider
+    {
+        private readonly string connectionString = @"Server=DESKTOP-DGUPQAR\VnHP;Database=JayBase;Trusted_Connection=True;";
+
+        private SqlConnection sqlConnection;
+
+       
+
+        public async Task AddPost(PostsModel post)
+        {
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                await sqlConnection.OpenAsync();
+                var dynamicParameters = new DynamicParameters();
+                dynamicParameters.Add("@IDauthor", post.Author.IDaccount);
+                dynamicParameters.Add("@Text", post.Text);
+                
+                dynamicParameters.Add("@LikesNumber", post.LikesNumber);
+                dynamicParameters.Add("@CommentNumber", post.CommentsNumber);
+                await sqlConnection.ExecuteAsync(
+                    "AddPost",
+                    dynamicParameters,
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+        public async Task<PostsModel> GetPost(int id)
+        {
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                await sqlConnection.OpenAsync();
+                var dynamicParameters = new DynamicParameters();
+                dynamicParameters.Add("@id", id);
+                return await sqlConnection.QuerySingleOrDefaultAsync<PostsModel>(
+                    "GetPost",
+                    dynamicParameters,
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+        public async Task<IEnumerable<PostsModel>> GetPosts()
+        {
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                await sqlConnection.OpenAsync();
+                return await sqlConnection.QueryAsync<PostsModel>(
+                    "GetPosts",
+                    null,
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+        public async Task UpdatePost(PostsModel post)
+        {
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                await sqlConnection.OpenAsync();
+                var dynamicParameters = new DynamicParameters();
+                dynamicParameters.Add("@IDauthor", post.Author.IDaccount);
+                dynamicParameters.Add("@Text", post.Text);
+          
+                dynamicParameters.Add("@LikesNumber", post.LikesNumber);
+                dynamicParameters.Add("@CommentNumber", post.CommentsNumber);
+                await sqlConnection.ExecuteAsync(
+                    "UpdatePost",
+                    dynamicParameters,
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+    }
+}
