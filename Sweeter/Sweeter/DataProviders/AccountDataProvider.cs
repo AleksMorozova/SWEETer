7d219c7657284dabe8dfb530aa3f4f90ad6bd6b7
Rufield <biglazycatukr@gmail.com>
2018-02-04 20:50:13 +0200

    Add Factory

--- Sweeter/Sweeter/DataProviders/AccountDataProvider.cs
+++ Sweeter/Sweeter/DataProviders/AccountDataProvider.cs
@@ -15,14 +15,11 @@ namespace Sweeter.DataProviders
     
     public class AccountDataProvider : IAccountDataProvider
     {
-        
-        string connectionString  =System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-       
-        private SqlConnection sqlConnection;
+        private ConnectionFactory F = new ConnectionFactory("DBconnecting");
 
         public void AddAccount(AccountModel account)
         {
-            using (var sqlConnection = new SqlConnection(connectionString))
+            using (var sqlConnection = F.CreateConnection)
             {
                 sqlConnection.Execute(@"insert into AccountTable(Name, Email, Password, Username, Avatar)
       values (@Name, @Email, @Password, @Username, @Avatar);",
@@ -34,7 +31,7 @@ namespace Sweeter.DataProviders
 
         public void DeleteAccount(int id)
         {
-            using (var sqlConnection = new SqlConnection(connectionString))
+            using (var sqlConnection = F.CreateConnection)
             {
                 sqlConnection.Execute(@"delete from AccountTable where IDuser = @id", new { id = id });
             }
@@ -42,7 +39,7 @@ namespace Sweeter.DataProviders
 
         public AccountModel GetAccount(int id)
         {
-            using (var sqlConnection = new SqlConnection(connectionString))
+            using (var sqlConnection = F.CreateConnection)
             {
                 var account = sqlConnection.Query<AccountModel>("select * from AccountTable where IDuser = @id", new { id = id }).First();
                 return account;
@@ -51,7 +48,7 @@ namespace Sweeter.DataProviders
 
         public AccountModel GetAccountByEmail(string Email)
         {
-            using (var sqlConnection = new SqlConnection(connectionString))
+            using (var sqlConnection = F.CreateConnection)
             {
                 var account = sqlConnection.Query<AccountModel>("select * from AccountTable where Email=@email", new { email = Email }).First();
                 return account;
@@ -59,7 +56,7 @@ namespace Sweeter.DataProviders
         }
         public IEnumerable<AccountModel> GetAccounts()
         {
-            using (var sqlConnection = new SqlConnection(connectionString))
+            using (var sqlConnection = F.CreateConnection)
             {
                 var accounts = sqlConnection.Query<AccountModel>("select * from AccountTable").ToList();
                 return accounts;
@@ -67,7 +64,7 @@ namespace Sweeter.DataProviders
         }
         public IEnumerable<AccountModel> GetAccountsByEmail(string Email)
         {
-            using (var sqlConnection = new SqlConnection(connectionString))
+            using (var sqlConnection = F.CreateConnection)
             {
                 var accounts = sqlConnection.Query<AccountModel>("select * from AccountTable where Email=@Email",new { Email = Email }).ToList();
                 return accounts;
@@ -75,7 +72,7 @@ namespace Sweeter.DataProviders
         }
         public IEnumerable<AccountModel> GetAccountsByUsername(string username)
         {
-            using (var sqlConnection = new SqlConnection(connectionString))
+            using (var sqlConnection = F.CreateConnection)
             {
                 var accounts = sqlConnection.Query<AccountModel>("select * from AccountTable where Username=@username", new { username=username }).ToList();
                 return accounts;
@@ -84,7 +81,7 @@ namespace Sweeter.DataProviders
 
         public void UpdateAccount(AccountModel account)
         {
-            using (var sqlConnection = new SqlConnection(connectionString))
+            using (var sqlConnection = F.CreateConnection)
             {
 
                 sqlConnection.Execute(@"update AccountTable set Fullname=@Fullname, Email=@Email, Password=@Password, Username=@Username, Avatar=@Avatar where ID = @id;",
